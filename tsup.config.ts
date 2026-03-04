ba7aff508e69dfe777487f70f612a0ad582bf3cc
Max Blum <maxblum94@yahoo.de>
2026-03-04 14:55:07 +0100

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm'],
-  target: 'node18',
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  target: "node18",
   clean: true,
   bundle: true,
   sourcemap: false,
+  outExtension: () => ({ js: ".js" }),
   banner: {
-    js: '#!/usr/bin/env node',
+    js: "#!/usr/bin/env node",
   },
-})
+});
